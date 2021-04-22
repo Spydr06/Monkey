d574400586868c3f4db0ce7517372a7ba9589b93
Spydr06 <felix-wagenknecht@web.de>
2021-04-22 22:08:14 +0200

    Own addition: exit, puts and gets functions

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
 var builtins = map[string]*object.Builtin{
 	"len": {
 		Fn: func(args ...object.Object) object.Object {
@@ -36,20 +38,42 @@ var builtins = map[string]*object.Builtin{
 	"gets": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) > 1 {
-				return newError("wrong number of arguments. got=%d, wand=0, 1", len(args))
+				return newError("wrong number of arguments. got=%d, want=0, 1", len(args))
 			}
 
 			if len(args) > 0 && args[0] != nil {
 				fmt.Println(args[0].Inspect())
 			}
 
-			scanner := bufio.NewScanner(os.Stdin)
 			scanned := scanner.Scan()
 			if !scanned {
 				return newError("could not scan properly")
 			}
 
 			return &object.String{Value: scanner.Text()}
+		},
+	},
+	"exit": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 && len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=1, 2", len(args))
+			}
+
+			_, ok := args[0].(*object.Integer)
+			if !ok {
+				return newError("argument to `exit` not supported, got=%s", args[0].Type())
+			}
+
+			if len(args) > 1 {
+				_, ok := args[1].(*object.String)
+				if !ok {
+					return newError("argument to `exit` not supported, got=%s", args[1].Type())
+				}
+				fmt.Println("Exit: " + args[1].Inspect())
+			}
+
+			os.Exit(int(args[0].(*object.Integer).Value))
+			return NULL
 		},
 	},
 }
